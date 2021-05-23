a6f086bd5f86ac7bee33e9e2b773b608de3bc64b
ruofanliu <kelseyliu1988@gmail.com>
2021-05-23 11:32:32 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # Phishpedia
-Official Implementation of "Phishpedia: A Hybrid Deep Learning Based Approach to Visually Identify Phishing Webpages" USENIX'21 
+
+- This is the official implementation of "Phishpedia: A Hybrid Deep Learning Based Approach to Visually Identify Phishing Webpages" USENIX'21 [[paper](https://www.usenix.org/conference/usenixsecurity21/presentation/lin)]
+    
+### [Framework]
+    
+<img src="big_pic/pic.png" style="width:2000px;height:350px"/>
+
+```Input```: A URL and its screenshot ```Output```: Phish/Benign, Phishing target
+- Step 1: Enter <b>Deep Object Detection Model</b>, get predicted elements
+
+- Step 2: Enter <b>Deep Siamese Model</b>
+    - If Siamese report no target, ```Return  Benign, None```
+    - Else Siamese report a target, ```Return Phish, Phishing target``` 
+
+
+### [Instructions]
+- Download all the model files:
+- First ```cd src/phishpedia```
+``` wget https://drive.google.com/file/d/1H0Q_DbdKPLFcZee8I14K62qV7TTy7xvS/view?usp=sharing```
+``` wget https://drive.google.com/file/d/1_C8NSQYWkpW_-tW8WzFaBr8vDeBAWQ87/view?usp=sharing```
+``` wget https://drive.google.com/file/d/1qSdkSSoCYUkZMKs44Rup_1DPBxHnEKl1/view?usp=sharing```
+- Then ```cd src/detectron2_pedia/output/rcnn_2```
+```wget https://drive.google.com/file/d/1tE2Mu5WC8uqCxei3XqAd7AWaP5JTmVWH/view?usp=sharing```
+
+- For phish discovery experiment:
+```
+python phishpedia_main.py --folder [data folder you want to test] --results [xxx.txt]
+```
+- For general experiment, download [[phishing_30k](https://drive.google.com/file/d/12ypEMPRQ43zGRqHGut0Esq2z5en0DH4g/view?usp=sharing)] and [[benign_30k](https://drive.google.com/file/d/1yORUeSrF5vGcgxYrsCoqXcpOUHt-iHq_/view?usp=sharing)] dataset, put them under **datasets/**:
+please run evaluation scripts
+```
+python -m src.pipeline_eval --data-dir datasets/phish_sample_30k --mode phish --write-txt output_phish.txt --ts [threshold for siamese]
+```
+```
+python -m src.pipeline_eval --data-dir datasets/benign_sample_30k --mode benign --write-txt output_benign.txt --ts [threshold for siamese]
+```
+- For adversarial attack on Siamese, download all files [[here]()] and put them under **datasets/**:
+
+### [Project structure]
+- src
+    - phishpedia: training script for siamese
+    - element_detector: training script for element detector
+    - util: other scripts (chromedriver utilities)
+    - element_detector.py: main script for element detector
+    - siamese.py: main script for siamese
+
+        
---
+++ src/adv_attack/attack/JSMA.py
@@ -0,0 +1,107 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd.gradcheck import zero_gradients
+from torch.autograd import Variable
+
+import numpy as np
+import copy
+
+
+def compute_jacobian(model, num_classes, inputs, output):
+    '''
+    'Helper function: compute jacobian matrix of confidence score vector w.r.t. input
+    :param model: subject model
+    :param num_classes:
+    :param inputs:
+    :param output:
+    :return:
+    '''
+
+    jacobian = torch.zeros(num_classes, *inputs.size()).cuda()
+
+    for i in range(num_classes):
+        zero_gradients(inputs)
+        model.zero_grad()
+        output[0, i].backward(retain_graph=True)
+        jacobian[i] = inputs.grad.data
+
+    return torch.transpose(jacobian, dim0=0, dim1=1)
+
+
+
+def saliency_map(jacobian, search_space, target_index):
+    '''
+    Helper function: compute saliency map and select the maximum index
+    :param jacobian: jocabian matrix
+    :param search_space: ignore some regions
+    :param target_index: take one column of jacobian
+    :return:
+    '''
+    
+    jacobian = jacobian.squeeze(0)
+    alpha = jacobian[target_index].sum(0).sum(0)
+    beta = jacobian.sum(0).sum(0) - alpha
+
+    # filter by the sign of alpha and beta
+    mask1 = torch.ge(alpha, 0.0)
+    mask2 = torch.le(beta, 0.0)
+    mask = torch.mul(torch.mul(mask1, mask2), search_space)
+    saliency_map = torch.mul(torch.mul(alpha, torch.abs(beta)), mask.float())
+
+    # get the maximum index
+    row_idx, col_idx = (saliency_map == torch.max(saliency_map)).nonzero()[0]
+    return row_idx, col_idx
+
+
+def jsma(model, num_classes, image, target, max_iter=100, clip_min=-1.0, clip_max=1.0):
+    '''
+    https://github.com/ast0414/adversarial-example/blob/master/craft.py
+    Saliency map attack
+
+    :param model: subject model
+    :param num_classes: number of classes in classification
+    :param image: input image
+    :param target: original class
+    :param max_iter: maximum iteration allowed
+    :param clip_min: clip image into legal range
+    :param clip_max:
+    :return:
+    '''
+
+    # Make a clone since we will alter the values
+    pert_image = copy.deepcopy(image)
+    x = Variable(pert_image, requires_grad=True)
+
+    output = model(x)
+    label = output.max(1, keepdim=True)[1]
+
+    count = 0
+    
+    # if attack is successful or reach the maximum number of iterations
+    while label != target:
+
+        # Skip the pixels that have been attacked before
+        search_space = (x.data[0].sum(0) > clip_min*x.data.shape[1]) & (x.data[0].sum(0) < clip_max*x.data.shape[1])
+
+        # Calculate Jacobian
+        jacobian = compute_jacobian(model, num_classes, x, output)
+
+        # get the highest saliency map's index
+        row_idx, col_idx = saliency_map(jacobian, search_space, target)
+
+        # increase to its maximum value
+        x.data[0, :, row_idx, col_idx] = clip_max
+
+        # recompute prediction
+        output = model(x)
+        label = output.max(1, keepdim=True)[1]
+
+        count += 1
+        if count >= max_iter:
+            break
+
+    return x.data
+
+
