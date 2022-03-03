40167d1a4137ebf5c1c749dcdcd9646ba4878a07
ruofanliu <kelseyliu1988@gmail.com>
2022-03-03 15:52:56 +0800

    m

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(name='phishpedia',
       author_email='liu.ruofan16@u.nus.edu',
       url='https://github.com/lindsey98/Phishpedia',
       license='Apache License 2.0',
-      python_requires='=3.7',
+      python_requires='==3.7',
       install_requires=[
           'torchsummary'
             'scipy'
