52256f721f3b01033c83424f822337cf2b0e7aa5
ruofanliu <kelseyliu1988@gmail.com>
2022-03-03 20:50:24 +0800

    refactor

--- phishpedia/phishpedia_config.py
+++ phishpedia/phishpedia_config.py
@@ -1,6 +1,6 @@
 # Global configuration
-from .siamese import *
-from .detectron2_pedia.inference import *
+from src.siamese import *
+from src.detectron2_pedia.inference import *
 
 # element recognition model -- logo only
 cfg_path = './src/detectron2_pedia/configs/faster_rcnn.yaml'
