f427daf74cb0eebc45815e5d32c8dba637e48107
ruofanliu <kelseyliu1988@gmail.com>
2022-03-04 10:10:53 +0800

    refactor

--- phishpedia/__init__.py
+++ phishpedia/__init__.py
@@ -1,3 +1,5 @@
 # This is so that you can import ppack or import average from ppack
 # in stead of from ppack.functions import average
 
+from .phishpedia_config import *
+from .phishpedia_main import *
