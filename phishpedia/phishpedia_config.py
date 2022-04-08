0817f90a92cdeeb696df840983de6644f363acbc
ruofanliu <kelseyliu1988@gmail.com>
2022-04-08 11:57:55 +0800

    bug fix

--- phishpedia/phishpedia_config.py
+++ phishpedia/phishpedia_config.py
@@ -14,8 +14,8 @@ def load_config(cfg_path: Union[str, None]):
             configs = yaml.load(file, Loader=yaml.FullLoader)
 
         # element recognition model -- logo only
-        ELE_CFG_PATH = os.path.join(os.path.dirname(__file__), configs['ELE_MODEL']['CFG_PATH'])
-        ELE_WEIGHTS_PATH = os.path.join(os.path.dirname(__file__), configs['ELE_MODEL']['WEIGHTS_PATH'])
+        ELE_CFG_PATH = os.path.join(os.path.dirname(__file__), configs['ELE_MODEL']['CFG_PATH'].replace('/', os.sep))
+        ELE_WEIGHTS_PATH = os.path.join(os.path.dirname(__file__), configs['ELE_MODEL']['WEIGHTS_PATH'].replace('/', os.sep))
         ELE_CONFIG_THRE = configs['ELE_MODEL']['DETECT_THRE']
         ELE_MODEL = config_rcnn(ELE_CFG_PATH, ELE_WEIGHTS_PATH, conf_threshold=ELE_CONFIG_THRE)
 
@@ -25,30 +25,30 @@ def load_config(cfg_path: Union[str, None]):
         print('Load protected logo list')
         if configs['SIAMESE_MODEL']['TARGETLIST_PATH'].endswith('.zip') \
                 and not os.path.isdir('{}'.format(os.path.join(os.path.dirname(__file__),
-                                                               configs['SIAMESE_MODEL']['TARGETLIST_PATH'].split('.zip')[0]))
+                                                               configs['SIAMESE_MODEL']['TARGETLIST_PATH'].split('.zip')[0]).replace('/', os.sep))
                                       ):
             subprocess.run(
-                "unzip {} -d {}".format(os.path.join(os.path.dirname(__file__),  configs['SIAMESE_MODEL']['TARGETLIST_PATH']),
-                                        os.path.join(os.path.dirname(__file__), configs['SIAMESE_MODEL']['TARGETLIST_PATH'].split('.zip')[0], "")
+                "unzip {} -d {}".format(os.path.join(os.path.dirname(__file__),  configs['SIAMESE_MODEL']['TARGETLIST_PATH'].replace('/', os.sep)),
+                                        os.path.join(os.path.dirname(__file__), configs['SIAMESE_MODEL']['TARGETLIST_PATH'].split('.zip')[0].replace('/', os.sep), "")
                                         ),
                 shell=True,
             )
 
         SIAMESE_MODEL, LOGO_FEATS, LOGO_FILES = phishpedia_config(
             num_classes=configs['SIAMESE_MODEL']['NUM_CLASSES'],
-            weights_path=os.path.join(os.path.dirname(__file__), configs['SIAMESE_MODEL']['WEIGHTS_PATH']),
+            weights_path=os.path.join(os.path.dirname(__file__), configs['SIAMESE_MODEL']['WEIGHTS_PATH'].replace('/', os.sep)),
             targetlist_path=os.path.join(os.path.dirname(__file__),
-                                         configs['SIAMESE_MODEL']['TARGETLIST_PATH'].split('.zip')[0]))
+                                         configs['SIAMESE_MODEL']['TARGETLIST_PATH'].split('.zip')[0].replace('/', os.sep)))
         print('Finish loading protected logo list')
-        DOMAIN_MAP_PATH = os.path.join(os.path.dirname(__file__), configs['SIAMESE_MODEL']['DOMAIN_MAP_PATH'])
+        DOMAIN_MAP_PATH = os.path.join(os.path.dirname(__file__), configs['SIAMESE_MODEL']['DOMAIN_MAP_PATH'].replace('/', os.sep))
 
     #################### '''Customized''' ####################
     else:
         with open(cfg_path) as file:
             configs = yaml.load(file, Loader=yaml.FullLoader)
 
-        ELE_CFG_PATH = configs['ELE_MODEL']['CFG_PATH']
-        ELE_WEIGHTS_PATH = configs['ELE_MODEL']['WEIGHTS_PATH']
+        ELE_CFG_PATH = configs['ELE_MODEL']['CFG_PATH'].replace('/', os.sep)
+        ELE_WEIGHTS_PATH = configs['ELE_MODEL']['WEIGHTS_PATH'].replace('/', os.sep)
         ELE_CONFIG_THRE = configs['ELE_MODEL']['DETECT_THRE']
         ELE_MODEL = config_rcnn(ELE_CFG_PATH, ELE_WEIGHTS_PATH, conf_threshold=ELE_CONFIG_THRE)
 
@@ -57,20 +57,20 @@ def load_config(cfg_path: Union[str, None]):
 
         print('Load protected logo list')
         if configs['SIAMESE_MODEL']['TARGETLIST_PATH'].endswith('.zip') \
-                and not os.path.isdir('{}'.format(configs['SIAMESE_MODEL']['TARGETLIST_PATH'].split('.zip')[0])):
+                and not os.path.isdir('{}'.format(configs['SIAMESE_MODEL']['TARGETLIST_PATH'].split('.zip')[0].replace('/', os.sep))):
             subprocess.run(
-                "unzip {} -d {}/".format(configs['SIAMESE_MODEL']['TARGETLIST_PATH'],
-                                         configs['SIAMESE_MODEL']['TARGETLIST_PATH'].split('.zip')[0]),
+                "unzip {} -d {}/".format(configs['SIAMESE_MODEL']['TARGETLIST_PATH'].replace('/', os.sep),
+                                         configs['SIAMESE_MODEL']['TARGETLIST_PATH'].split('.zip')[0].replace('/', os.sep)),
                 shell=True,
             )
 
         SIAMESE_MODEL, LOGO_FEATS, LOGO_FILES = phishpedia_config(
             num_classes=configs['SIAMESE_MODEL']['NUM_CLASSES'],
-            weights_path=configs['SIAMESE_MODEL']['WEIGHTS_PATH'],
-            targetlist_path=configs['SIAMESE_MODEL']['TARGETLIST_PATH'].split('.zip')[0])
+            weights_path=configs['SIAMESE_MODEL']['WEIGHTS_PATH'].replace('/', os.sep),
+            targetlist_path=configs['SIAMESE_MODEL']['TARGETLIST_PATH'].replace('/', os.sep).split('.zip')[0])
         print('Finish loading protected logo list')
 
-        DOMAIN_MAP_PATH = configs['SIAMESE_MODEL']['DOMAIN_MAP_PATH']
+        DOMAIN_MAP_PATH = configs['SIAMESE_MODEL']['DOMAIN_MAP_PATH'].replace('/', os.sep)
 
     return ELE_MODEL, SIAMESE_THRE, SIAMESE_MODEL, LOGO_FEATS, LOGO_FILES, DOMAIN_MAP_PATH
 
