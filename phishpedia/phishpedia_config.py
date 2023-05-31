e231283eecc4f3f437c74f78b265882bc11bf748
lindsey98 <kelseyliu1998@gmail.com>
2023-05-31 11:40:38 +0800

    fix

--- phishpedia/phishpedia_config.py
+++ phishpedia/phishpedia_config.py
@@ -1,6 +1,7 @@
 # Global configuration
 from phishpedia.src.siamese import *
 from phishpedia.src.detectron2_pedia.inference import *
+import phishpedia
 import subprocess
 from typing import Union
 import yaml
@@ -12,76 +13,34 @@ def load_config(cfg_path: Union[str, None], reload_targetlist=False):
     if cfg_path is None:
         with open(os.path.join(os.path.dirname(__file__), 'configs.yaml')) as file:
             configs = yaml.load(file, Loader=yaml.FullLoader)
-
-        # element recognition model -- logo only
-        ELE_CFG_PATH = os.path.join(os.path.dirname(__file__), configs['ELE_MODEL']['CFG_PATH'].replace('/', os.sep))
-        ELE_WEIGHTS_PATH = os.path.join(os.path.dirname(__file__), configs['ELE_MODEL']['WEIGHTS_PATH'].replace('/', os.sep))
-        ELE_CONFIG_THRE = configs['ELE_MODEL']['DETECT_THRE']
-        ELE_MODEL = config_rcnn(ELE_CFG_PATH, ELE_WEIGHTS_PATH, conf_threshold=ELE_CONFIG_THRE)
-
-        # siamese model
-        SIAMESE_THRE = configs['SIAMESE_MODEL']['MATCH_THRE']
-
-        print('Load protected logo list')
-        if configs['SIAMESE_MODEL']['TARGETLIST_PATH'].endswith('.zip') \
-                and not os.path.isdir('{}'.format(os.path.join(os.path.dirname(__file__),
-                                                               configs['SIAMESE_MODEL']['TARGETLIST_PATH'].split('.zip')[0]).replace('/', os.sep))
-                                      ):
-            subprocess.run(
-                "unzip {} -d {}".format(os.path.join(os.path.dirname(__file__),  configs['SIAMESE_MODEL']['TARGETLIST_PATH'].replace('/', os.sep)),
-                                        os.path.join(os.path.dirname(__file__), configs['SIAMESE_MODEL']['TARGETLIST_PATH'].split('.zip')[0].replace('/', os.sep), "")
-                                        ),
-                shell=True,
-            )
-
-        if os.path.exists(os.path.join(os.path.dirname(__file__), 'LOGO_FEATS.npy')) and reload_targetlist == False:
-            SIAMESE_MODEL = phishpedia_config_easy(num_classes=configs['SIAMESE_MODEL']['NUM_CLASSES'],
-                                                   weights_path=os.path.join(os.path.dirname(__file__), configs['SIAMESE_MODEL']['WEIGHTS_PATH'].replace('/', os.sep)))
-            LOGO_FEATS = np.load(os.path.join(os.path.dirname(__file__), 'LOGO_FEATS.npy'))
-            LOGO_FILES = np.load(os.path.join(os.path.dirname(__file__), 'LOGO_FILES.npy'))
-
-        else:
-            SIAMESE_MODEL, LOGO_FEATS, LOGO_FILES = phishpedia_config(
-                num_classes=configs['SIAMESE_MODEL']['NUM_CLASSES'],
-                weights_path=os.path.join(os.path.dirname(__file__), configs['SIAMESE_MODEL']['WEIGHTS_PATH'].replace('/', os.sep)),
-                targetlist_path=os.path.join(os.path.dirname(__file__),
-                                             configs['SIAMESE_MODEL']['TARGETLIST_PATH'].split('.zip')[0].replace('/', os.sep)))
-
-            np.save(os.path.join(os.path.dirname(__file__), 'LOGO_FEATS'), LOGO_FEATS)
-            np.save(os.path.join(os.path.dirname(__file__), 'LOGO_FILES'), LOGO_FILES)
-
-        print('Finish loading protected logo list')
-        DOMAIN_MAP_PATH = os.path.join(os.path.dirname(__file__), configs['SIAMESE_MODEL']['DOMAIN_MAP_PATH'].replace('/', os.sep))
-
-    #################### '''Customized''' ####################
     else:
         with open(cfg_path) as file:
             configs = yaml.load(file, Loader=yaml.FullLoader)
 
-        ELE_CFG_PATH = configs['ELE_MODEL']['CFG_PATH'].replace('/', os.sep)
-        ELE_WEIGHTS_PATH = configs['ELE_MODEL']['WEIGHTS_PATH'].replace('/', os.sep)
-        ELE_CONFIG_THRE = configs['ELE_MODEL']['DETECT_THRE']
-        ELE_MODEL = config_rcnn(ELE_CFG_PATH, ELE_WEIGHTS_PATH, conf_threshold=ELE_CONFIG_THRE)
+    ELE_CFG_PATH = configs['ELE_MODEL']['CFG_PATH'].replace('/', os.sep)
+    ELE_WEIGHTS_PATH = configs['ELE_MODEL']['WEIGHTS_PATH'].replace('/', os.sep)
+    ELE_CONFIG_THRE = configs['ELE_MODEL']['DETECT_THRE']
+    ELE_MODEL = config_rcnn(ELE_CFG_PATH, ELE_WEIGHTS_PATH, conf_threshold=ELE_CONFIG_THRE)
 
-        # siamese model
-        SIAMESE_THRE = configs['SIAMESE_MODEL']['MATCH_THRE']
+    # siamese model
+    SIAMESE_THRE = configs['SIAMESE_MODEL']['MATCH_THRE']
 
-        print('Load protected logo list')
-        if configs['SIAMESE_MODEL']['TARGETLIST_PATH'].endswith('.zip') \
-                and not os.path.isdir('{}'.format(configs['SIAMESE_MODEL']['TARGETLIST_PATH'].split('.zip')[0].replace('/', os.sep))):
-            subprocess.run(
-                "unzip {} -d {}/".format(configs['SIAMESE_MODEL']['TARGETLIST_PATH'].replace('/', os.sep),
-                                         configs['SIAMESE_MODEL']['TARGETLIST_PATH'].split('.zip')[0].replace('/', os.sep)),
-                shell=True,
-            )
+    print('Load protected logo list')
+    if configs['SIAMESE_MODEL']['TARGETLIST_PATH'].endswith('.zip') \
+            and not os.path.isdir('{}'.format(configs['SIAMESE_MODEL']['TARGETLIST_PATH'].split('.zip')[0].replace('/', os.sep))):
+        subprocess.run(
+            "unzip {} -d {}/".format(configs['SIAMESE_MODEL']['TARGETLIST_PATH'].replace('/', os.sep),
+                                     configs['SIAMESE_MODEL']['TARGETLIST_PATH'].split('.zip')[0].replace('/', os.sep)),
+            shell=True,
+        )
 
-        SIAMESE_MODEL, LOGO_FEATS, LOGO_FILES = phishpedia_config(
-            num_classes=configs['SIAMESE_MODEL']['NUM_CLASSES'],
-            weights_path=configs['SIAMESE_MODEL']['WEIGHTS_PATH'].replace('/', os.sep),
-            targetlist_path=configs['SIAMESE_MODEL']['TARGETLIST_PATH'].replace('/', os.sep).split('.zip')[0])
-        print('Finish loading protected logo list')
+    SIAMESE_MODEL, LOGO_FEATS, LOGO_FILES = phishpedia_config(
+        num_classes=configs['SIAMESE_MODEL']['NUM_CLASSES'],
+        weights_path=configs['SIAMESE_MODEL']['WEIGHTS_PATH'].replace('/', os.sep),
+        targetlist_path=configs['SIAMESE_MODEL']['TARGETLIST_PATH'].replace('/', os.sep).split('.zip')[0])
+    print('Finish loading protected logo list')
 
-        DOMAIN_MAP_PATH = configs['SIAMESE_MODEL']['DOMAIN_MAP_PATH'].replace('/', os.sep)
+    DOMAIN_MAP_PATH = configs['SIAMESE_MODEL']['DOMAIN_MAP_PATH'].replace('/', os.sep)
 
     return ELE_MODEL, SIAMESE_THRE, SIAMESE_MODEL, LOGO_FEATS, LOGO_FILES, DOMAIN_MAP_PATH
 
--- setup.sh
+++ setup.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
+FILEDIR=$(pwd)
+
 # Source the Conda configuration
 CONDA_BASE=$(conda info --base)
 source "$CONDA_BASE/etc/profile.d/conda.sh"
 
-# Create a new conda environment with Python 3.7
-ENV_NAME="myenv"
+# # Create a new conda environment with Python 3.7
+ENV_NAME="py37"
 
 # Check if the environment already exists
 conda info --envs | grep -w "$ENV_NAME" > /dev/null
 
 if [ $? -eq 0 ]; then
-    # If the environment exists, activate it
-    echo "Activating Conda environment $ENV_NAME"
-    conda activate "$ENV_NAME"
+   # If the environment exists, activate it
+   echo "Activating Conda environment $ENV_NAME"
+   conda activate "$ENV_NAME"
 else
-    # If the environment doesn't exist, create it with Python 3.7 and activate it
-    echo "Creating and activating new Conda environment $ENV_NAME with Python 3.7"
-    conda create -n "$ENV_NAME" python=3.7
-    conda activate "$ENV_NAME"
+   # If the environment doesn't exist, create it with Python 3.7 and activate it
+   echo "Creating and activating new Conda environment $ENV_NAME with Python 3.7"
+   conda create -n "$ENV_NAME" python=3.7
+   conda activate "$ENV_NAME"
 fi
 
 mkl_path=$(conda info --base)/envs/"$ENV_NAME"/lib
@@ -26,25 +28,25 @@ echo "MKL path is $mkl_path"
 # Export the LD_LIBRARY_PATH environment variable
 export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$mkl_path"
 
+
 # Get the CUDA and cuDNN versions, install pytorch, torchvision
 pip install -r requirements.txt
 conda install typing_extensions
-
 # Check if nvcc (CUDA compiler) is available
 if ! command -v nvcc &> /dev/null; then
     echo "CUDA is not available."
-    pip install torch==1.8.1 torchvision
+    pip install torch==1.9.0 torchvision
     # Install Detectron2
-    python -m --user pip install 'git+https://github.com/facebookresearch/detectron2.git'
+    python -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
 else
     cuda_version=$(nvcc --version | grep release | awk '{print $6}' | cut -c2- | awk -F. '{print $1"."$2}')
-    pip install torch==1.8.1 torchvision -f "https://download.pytorch.org/whl/cu${cuda_version//.}/torch_stable.html"
+    pip install torch==1.9.0 torchvision -f "https://download.pytorch.org/whl/cu${cuda_version//.}/torch_stable.html"
     # Install Detectron2
     cuda_version=$(nvcc --version | grep release | awk '{print $6}' | cut -c2- | awk -F. '{print $1$2}')
     case $cuda_version in
         "111" | "102" | "101")
           python -m pip install detectron2 -f \
-      https://dl.fbaipublicfiles.com/detectron2/wheels/cu"$cuda_version"/torch1.8/index.html
+      https://dl.fbaipublicfiles.com/detectron2/wheels/cu"$cuda_version"/torch1.9/index.html
         ;;
         *)
           echo "Please build Detectron2 from source https://detectron2.readthedocs.io/en/latest/tutorials/install.html">&2
@@ -53,10 +55,8 @@ else
     esac
 fi
 
-
 ## Download models
 export LD_LIBRARY_PATH=""
-pip install git+https://github.com/lindsey98/Phishpedia.git
 package_location=$(pip show phishpedia | grep Location | awk '{print $2}')
 
 if [ -z "Phishpedia" ]; then
@@ -88,5 +88,10 @@ else
   wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id='$file_id -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$file_id" -O "$output_file" && rm -rf /tmp/cookies.txt
   
 fi
+
+# Replace the placeholder in the YAML template
+
+sed "s|CONDA_ENV_PATH_PLACEHOLDER|$package_location/phishpedia|g" "$FILEDIR/phishpedia/configs_template.yaml" > "$FILEDIR/phishpedia/configs.yaml"
+
 
 echo "All packages installed successfully!"
