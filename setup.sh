a7ab5a91a2cdb0aabc0f058bf33462b6013de339
lindsey98 <kelseyliu1998@gmail.com>
2023-05-31 21:14:01 +0800

    fix

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ CONDA_BASE=$(conda info --base)
 source "$CONDA_BASE/etc/profile.d/conda.sh"
 
 # # Create a new conda environment with Python 3.7
-ENV_NAME="py37"
+ENV_NAME="myenv"
 
 # Check if the environment already exists
 conda info --envs | grep -w "$ENV_NAME" > /dev/null
@@ -19,7 +19,7 @@ if [ $? -eq 0 ]; then
 else
    # If the environment doesn't exist, create it with Python 3.7 and activate it
    echo "Creating and activating new Conda environment $ENV_NAME with Python 3.7"
-   conda create -n "$ENV_NAME" python=3.7
+   conda create -n "$ENV_NAME" python=3.8
    conda activate "$ENV_NAME"
 fi
 
@@ -31,32 +31,18 @@ export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$mkl_path"
 
 # Get the CUDA and cuDNN versions, install pytorch, torchvision
 pip install -r requirements.txt
+pip install cryptography==38.0.4
 conda install typing_extensions
-# Check if nvcc (CUDA compiler) is available
-if ! command -v nvcc &> /dev/null; then
-    echo "CUDA is not available."
-    pip install torch==1.9.0 torchvision
-    # Install Detectron2
-    python -m pip install 'git+https://github.com/facebookresearch/detectron2.git'
-else
-    cuda_version=$(nvcc --version | grep release | awk '{print $6}' | cut -c2- | awk -F. '{print $1"."$2}')
-    pip install torch==1.9.0 torchvision -f "https://download.pytorch.org/whl/cu${cuda_version//.}/torch_stable.html"
-    # Install Detectron2
-    cuda_version=$(nvcc --version | grep release | awk '{print $6}' | cut -c2- | awk -F. '{print $1$2}')
-    case $cuda_version in
-        "111" | "102" | "101")
-          python -m pip install detectron2 -f \
-      https://dl.fbaipublicfiles.com/detectron2/wheels/cu"$cuda_version"/torch1.9/index.html
-        ;;
-        *)
-          echo "Please build Detectron2 from source https://detectron2.readthedocs.io/en/latest/tutorials/install.html">&2
-          exit 1
-          ;;
-    esac
-fi
+pip install torch==1.9.0 torchvision -f \
+  "https://download.pytorch.org/whl/cu111/torch_stable.html"
+
+python -m pip install detectron2 -f \
+  https://dl.fbaipublicfiles.com/detectron2/wheels/cu111/torch1.9/index.html
+
 
 ## Download models
 export LD_LIBRARY_PATH=""
+pip install git+https://github.com/lindsey98/Phishpedia.git
 package_location=$(pip show phishpedia | grep Location | awk '{print $2}')
 
 if [ -z "Phishpedia" ]; then
