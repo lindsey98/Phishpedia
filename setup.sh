77c5678820397e8aacd617de94b8efb6472c2ebc
lindsey98 <51521323+lindsey98@users.noreply.github.com>
2023-11-08 09:49:45 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,24 +1,5 @@
 #!/bin/bash
 
-# # Set Conda environment as an environment variable
-# # export MYENV=$(conda info --base)/envs/"$ENV_NAME"
-
-# # Get the CUDA and cuDNN versions, install pytorch, torchvision
-# conda run -n "$ENV_NAME" pip install -r requirements.txt
-
-# # Install pytorch, torchvision, detectron2
-# if command -v nvcc &> /dev/null; then
-#    conda run -n "$ENV_NAME" pip install torch==1.9.0 torchvision -f "https://download.pytorch.org/whl/cu111/torch_stable.html"
-#    conda run -n "$ENV_NAME" python -m pip install detectron2 -f "https://dl.fbaipublicfiles.com/detectron2/wheels/cu111/torch1.9/index.html"
-# else
-#    conda run -n "$ENV_NAME" pip install torch==1.9.0+cpu torchvision==0.10.0+cpu torchaudio==0.9.0 -f "https://download.pytorch.org/whl/torch_stable.html"
-#    conda run -n "$ENV_NAME" python -m pip install detectron2 -f "https://dl.fbaipublicfiles.com/detectron2/wheels/cpu/torch1.9/index.html"
-# fi
-
-# ## Download models
-# conda run -n "$ENV_NAME" pip install -v .
-# package_location=$(conda run -n "$ENV_NAME" pip show phishpedia | grep Location | awk '{print $2}')
-
 FILEDIR=$(pwd)
 conda info --envs | grep -w "$ENV_NAME" > /dev/null
 
@@ -33,13 +14,20 @@ fi
 
 pip install -r requirements.txt
 
-# Install pytorch, torchvision, detectron2
-if command -v nvcc &> /dev/null; then
-   pip install torch==1.9.0 torchvision -f "https://download.pytorch.org/whl/cu111/torch_stable.html"
-   python -m pip install detectron2 -f "https://dl.fbaipublicfiles.com/detectron2/wheels/cu111/torch1.9/index.html"
+OS=$(uname -s)
+
+if [[ "$OS" == "Darwin" ]]; then
+  echo "Installing PyTorch and torchvision for macOS."
+  pip install torch==1.9.0 torchvision==0.10.0 torchaudio==0.9.0
 else
-   pip install torch==1.9.0+cpu torchvision==0.10.0+cpu torchaudio==0.9.0 -f "https://download.pytorch.org/whl/torch_stable.html"
-   python -m pip install detectron2 -f "https://dl.fbaipublicfiles.com/detectron2/wheels/cpu/torch1.9/index.html"
+  # Check if NVIDIA GPU is available for Linux and Windows
+  if command -v nvcc &> /dev/null; then
+    echo "CUDA is detected, installing GPU-supported PyTorch and torchvision."
+    pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+  else
+    echo "No CUDA detected, installing CPU-only PyTorch and torchvision."
+    pip install torch==1.9.0+cpu torchvision==0.10.0+cpu torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+  fi
 fi
 
 ## Download models
