897efa40d1cbfe7f5babef435480905a8656c65c
lindsey98 <kelseyliu1998@gmail.com>
2024-01-26 16:45:08 +0800

    remove unused

--- phishpedia/__init__.py
+++
@@ -1,5 +0,0 @@
-# This is so that you can import ppack or import average from ppack
-# in stead of from ppack.functions import average
-
-from .phishpedia_config import *
-from .phishpedia_main import *
